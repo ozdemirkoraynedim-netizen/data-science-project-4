8a8aa382cd3f2eb45a511f7d22221c1e481d64ce
Koray <ozdemirkoraynedim@gmail.com>
2026-09-22 20:43:44 +0300

    Complete SQL project 4

--- data/question.py
+++ data/question.py
@@ -9,37 +9,73 @@ def connect_db():
         port=5432,
         database="postgres",
         user="postgres",
-        password=password
+        password=229922992299
     )
 
 def create_view_completed_orders():
     with connect_db() as conn:
         with conn.cursor() as cur:
-            cur.execute("""""")
+            cur.execute("""
+                CREATE OR REPLACE VIEW completed_orders AS
+                SELECT *
+                FROM orders
+                WHERE status = 'completed';
+            """)
             conn.commit()
 
 def create_view_electronics_products():
     with connect_db() as conn:
         with conn.cursor() as cur:
-            cur.execute("""""")
+            cur.execute("""CREATE OR REPLACE VIEW electronics_products AS
+SELECT *
+FROM products
+WHERE category = 'Electronics';""")
             conn.commit()
 
 def total_spending_per_customer():
     with connect_db() as conn:
         with conn.cursor() as cur:
-            cur.execute("""""")
+            cur.execute("""SELECT
+        c.full_name,
+        SUM(o.quantity * p.price) AS total_spending
+    FROM customers c
+    JOIN orders o
+        ON c.customer_id = o.customer_id
+    JOIN products p
+        ON o.product_id = p.product_id
+    GROUP BY c.customer_id, c.full_name;""")
             return cur.fetchall()
 
 def order_details_with_total():
     with connect_db() as conn:
         with conn.cursor() as cur:
-            cur.execute("""""")
+            cur.execute("""
+    SELECT
+        o.order_id,
+        c.full_name,
+        p.product_name,
+        o.quantity * p.price AS total
+    FROM orders o
+    JOIN customers c
+        ON o.customer_id = c.customer_id
+    JOIN products p
+        ON o.product_id = p.product_id;
+""")
             return cur.fetchall()
 
 def get_customer_who_bought_most_expensive_product():
     conn = connect_db()
     cur = conn.cursor()
-    cur.execute("""""")
+    cur.execute("""
+    SELECT c.full_name
+    FROM orders o
+    JOIN customers c
+        ON o.customer_id = c.customer_id
+    JOIN products p
+        ON o.product_id = p.product_id
+    ORDER BY p.price DESC
+    LIMIT 1;
+""")
     result = cur.fetchall()
     cur.close()
     conn.close()
@@ -49,7 +85,16 @@ def get_customer_who_bought_most_expensive_product():
 def get_order_status_descriptions():
     conn = connect_db()
     cur = conn.cursor()
-    cur.execute("""""")
+    cur.execute("""
+    SELECT
+        order_id,
+        status,
+        CASE
+            WHEN status = 'completed' THEN 'Tamamlandı'
+            WHEN status = 'cancelled' THEN 'İptal Edildi'
+        END AS status_description
+    FROM orders;
+""")
     result = cur.fetchall()
     cur.close()
     conn.close()
@@ -59,7 +104,14 @@ def get_order_status_descriptions():
 def get_products_above_average_price():
     conn = connect_db()
     cur = conn.cursor()
-    cur.execute("""""")
+    cur.execute("""
+    SELECT product_name, price
+    FROM products
+    WHERE price > (
+        SELECT AVG(price)
+        FROM products
+    );
+""")
     result = cur.fetchall()
     cur.close()
     conn.close()
@@ -69,7 +121,16 @@ def get_products_above_average_price():
 def get_customer_categories():
     conn = connect_db()
     cur = conn.cursor()
-    cur.execute("""""")
+    cur.execute("""SELECT
+    c.full_name,
+    CASE
+        WHEN COUNT(o.order_id) >= 3 THEN 'Sadık Müşteri'
+        ELSE 'Yeni Müşteri'
+    END AS customer_category
+FROM customers c
+LEFT JOIN orders o
+    ON c.customer_id = o.customer_id
+GROUP BY c.customer_id, c.full_name;""")
     result = cur.fetchall()
     cur.close()
     conn.close()
@@ -79,7 +140,11 @@ def get_customer_categories():
 def get_recent_customers():
     conn = connect_db()
     cur = conn.cursor()
-    cur.execute("""""")
+    cur.execute("""SELECT c.full_name
+FROM orders o
+JOIN customers c
+    ON o.customer_id = c.customer_id
+WHERE o.order_date >= CURRENT_DATE - INTERVAL '30 days';""")
     result = cur.fetchall()
     cur.close()
     conn.close()
@@ -89,7 +154,15 @@ def get_recent_customers():
 def get_most_ordered_product():
     conn = connect_db()
     cur = conn.cursor()
-    cur.execute("""""")
+    cur.execute("""SELECT
+    p.product_name,
+    COUNT(o.order_id) AS order_count
+FROM orders o
+JOIN products p
+    ON o.product_id = p.product_id
+GROUP BY p.product_id, p.product_name
+ORDER BY COUNT(o.order_id) DESC
+LIMIT 1;""")
     result = cur.fetchall()
     cur.close()
     conn.close()
@@ -99,7 +172,16 @@ def get_most_ordered_product():
 def get_product_price_categories():
     conn = connect_db()
     cur = conn.cursor()
-    cur.execute("""""")
+    cur.execute("""
+    SELECT
+        product_name,
+        price,
+        CASE
+            WHEN price >= 1000 THEN 'Pahalı'
+            ELSE 'Ucuz'
+        END AS price_category
+    FROM products;
+""")
     result = cur.fetchall()
     cur.close()
     conn.close()
